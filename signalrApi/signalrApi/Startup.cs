a147436838bb62d505e60e9235ab00d058485ee5
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-29 11:47:37 -0500

    added signalr to api

--- signalrApi/signalrApi/Startup.cs
+++ signalrApi/signalrApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using signalrApi.Hubs;
 
 namespace signalrApi
 {
@@ -26,6 +27,7 @@ namespace signalrApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,7 @@ namespace signalrApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
