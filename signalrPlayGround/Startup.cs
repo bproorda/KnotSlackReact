4e3da404cd28124d4e2bb0da7a4a57905e0fcb79
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-29 13:36:31 -0500

    tried messing with launch settings and startup, still cors error

--- signalrPlayGround/Startup.cs
+++ signalrPlayGround/Startup.cs
@@ -26,6 +26,14 @@ namespace signalrPlayGround
         {
             services.AddRazorPages();
             services.AddSignalR();
+            //services.AddCors();
+            services.AddCors(options => options.AddPolicy("CorsPolicy",
+          builder =>
+          {
+              builder.AllowAnyMethod().AllowAnyHeader()
+                     .WithOrigins("http://localhost:5001")
+                     .AllowCredentials();
+          }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,9 +53,13 @@ namespace signalrPlayGround
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+           
+
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
