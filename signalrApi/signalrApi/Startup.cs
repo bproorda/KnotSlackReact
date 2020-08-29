d8a8486fafa7ac992286d9aaba0a61947ca7286d
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-29 13:35:12 -0500

    running into cors errors, hmmm

--- signalrApi/signalrApi/Startup.cs
+++ signalrApi/signalrApi/Startup.cs
@@ -28,6 +28,7 @@ namespace signalrApi
         {
             services.AddControllers();
             services.AddSignalR();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
