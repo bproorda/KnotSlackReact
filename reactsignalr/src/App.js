49acfaa412f57f6098b409dd88269f204683eda1
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-30 17:13:17 -0500

    added useState to app, started login component

--- reactsignalr/src/App.js
+++ reactsignalr/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/header';
+import Login from './components/login';
 import './App.css';
 
 function App() {
+const [userName, setUserName] = useState("");
+
   return (
     <>
     <Header />
- 
+    {userName ? <p>Hello, {userName}. Welcome to your chat!</p> : <Login />}
     </>
   );
 }
---
+++ reactsignalr/src/components/login/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default function Login(props){
+    return(
+        <>
+        <p>Log in Here</p>
+        </>
+    )
+}
