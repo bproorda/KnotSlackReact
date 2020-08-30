c15ba94a245aa91a9f0c351f8596fb6235d5967e
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-30 13:24:08 -0500

    created header

--- reactsignalr/src/App.js
+++ reactsignalr/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Work in progress!
-        </p>
-      </header>
-    </div>
+    <>
+    <Header />
+ 
+    </>
   );
 }
 
