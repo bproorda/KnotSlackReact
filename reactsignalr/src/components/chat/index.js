1e915d9c60d01397d021cd268828d7f65be193f3
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-30 20:29:44 -0500

    scaffolded sending out messages

--- reactsignalr/src/components/chat/index.js
+++ reactsignalr/src/components/chat/index.js
@@ -2,35 +2,38 @@ import React, { useState, useEffect, useRef } from 'react';
 import {
     HubConnectionBuilder,
     LogLevel,
-  } from '../../../node_modules/@microsoft/signalr/dist/browser/signalr'
+} from '../../../node_modules/@microsoft/signalr/dist/browser/signalr'
 
-export default function Chat(props){
+export default function Chat(props) {
 
-    //const {username} = props;
+    const {username} = props;
 
     const [hubConnection, setHubConnection] = useState();
-    const [ chat, setChat ] = useState([]);
+
+    const [chat, setChat] = useState([]);
+
+    const [message, setMessage] = useState("");
     const latestChat = useRef(null);
 
-    useEffect(()=>{
-      let hubConnection =  new HubConnectionBuilder()
-      .withUrl("https://localhost:5001/chatHub")
-      .configureLogging(LogLevel.Information)
-      .build();
+    useEffect(() => {
+        let hubConnection = new HubConnectionBuilder()
+            .withUrl("https://localhost:5001/chatHub")
+            .configureLogging(LogLevel.Information)
+            .build();
 
-      setHubConnection(hubConnection);
-    },[]);
+        setHubConnection(hubConnection);
+    }, []);
 
     useEffect(() => {
         if (hubConnection) {
             hubConnection.start()
                 .then(result => {
                     console.log('Connected!');
-    
+
                     hubConnection.on('ReceiveMessage', message => {
                         const updatedChat = [...latestChat.current];
                         updatedChat.push(message);
-                    
+
                         setChat(updatedChat);
                     });
                 })
@@ -38,9 +41,25 @@ export default function Chat(props){
         }
     }, [hubConnection]);
 
-    return(
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log("Send!")
+    };
+
+    const changeHandler = e => {
+        setMessage(e.target.value);
+    }
+
+    return (
         <>
-        <h2>Chat goes here</h2>
+            <form onSubmit={submitHandler}>
+                <label>
+                    {username} says:
+    <input type="text" name="name" onChange={changeHandler} />
+                </label>
+                <button name="name" type="submit">Send</button>
+            </form>
+            <p>{message}</p>
         </>
     )
 }
