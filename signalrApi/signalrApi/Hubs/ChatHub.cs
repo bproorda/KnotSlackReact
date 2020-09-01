4d19950475541ec15755ee66d93534ea14e8f51e
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-31 19:03:50 -0500

    added method for adding users to new list of users

--- signalrApi/signalrApi/Hubs/ChatHub.cs
+++ signalrApi/signalrApi/Hubs/ChatHub.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace signalrApi.Hubs
 {
     public class ChatHub : Hub
     {
+        private List<string> users;
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddUser(string user)
+        {
+            users.Add(user);
+            string[] usersArray = users.ToArray();
+
+            await Clients.All.SendAsync("AddUser", usersArray);
         }
     }
 }
