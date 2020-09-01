b3685e4b68d4aa30ef42d5facc607e9068b6dd41
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-08-31 20:41:54 -0500

    added more to dbContext

--- signalrApi/signalrApi/Data/knotSlackDbContext.cs
+++ signalrApi/signalrApi/Data/knotSlackDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using signalrApi.Models.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace signalrApi.Data
 {
-    public class knotSlackDbContext
+    public class knotSlackDbContext : IdentityDbContext<ksUser>
     {
+        public knotSlackDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<ksUser> ksUser { get; set; }
     }
 }
