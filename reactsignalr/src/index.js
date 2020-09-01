29491d5e2eda47ec7bfc7fcd3b3e47550338d51d
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-01 16:46:39 -0500

    wrapped app with auth

--- reactsignalr/src/index.js
+++ reactsignalr/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './contexts/auth.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
