a8d34f1d8cfa7808413302ea453e40f479c2bec3
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-03 19:30:07 -0500

    added react router dom, put browserrouter into index.js

--- reactsignalr/src/index.js
+++ reactsignalr/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/auth.js';
@@ -8,7 +9,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
+      <BrowserRouter>
     <App />
+    </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
