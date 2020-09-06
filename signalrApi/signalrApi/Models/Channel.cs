bc456dd3b3f9352ca515ffa9b0060dd7304e9346
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-06 15:34:10 -0500

    added model for channels

---
+++ signalrApi/signalrApi/Models/Channel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalrApi.Models
+{
+    public class Channel
+    {
+        [Key]
+        [Required]
+        public string Name { get; set; }
+
+        public List<string> Members { get; set; }
+
+    }
+}
