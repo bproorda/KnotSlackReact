71e636d97dc448a8e90e5ba4b51a28a0adca6a73
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-06 18:39:47 -0500

    added channel repository

---
+++ signalrApi/signalrApi/Repositories/ChannelRepos/ChannelRepository.cs
@@ -0,0 +1,33 @@
+﻿using signalrApi.Data;
+using signalrApi.Models;
+using signalrApi.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalrApi.Repositories.ChannelRepos
+{
+    public class ChannelRepository : IChannelRepository
+    {
+        private knotSlackDbContext _context;
+        private IUserManager userManager;
+
+        public ChannelRepository(knotSlackDbContext _context, IUserManager userManager)
+        {
+            this._context = _context;
+            this.userManager = userManager;
+        }
+        public async Task<List<string>> GetMyChannels(string username)
+        {
+            var user = await userManager.FindByNameAsync(username);
+
+            List<string> channels = new List<string>();
+
+            user.UserChannels.ForEach(uc => channels.Add(uc.ChannelName));
+
+            return channels;
+
+        }
+    }
+}
---
+++ signalrApi/signalrApi/Repositories/ChannelRepos/IChannelRepository.cs
@@ -0,0 +1,15 @@
+﻿using signalrApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalrApi.Repositories.ChannelRepos
+{
+   public interface IChannelRepository
+    {
+        Task<List<string>> GetMyChannels(string user);
+
+        
+    }
+}
