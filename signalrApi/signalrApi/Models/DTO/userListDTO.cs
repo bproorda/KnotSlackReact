ba44de11553e2233ec21d8524aa3c35d95e56109
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-05 21:17:27 -0500

    created new dto for user list

---
+++ signalrApi/signalrApi/Models/DTO/userListDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalrApi.Models.DTO
+{
+    public class userListDTO
+    {
+        public string Username { get; set; }
+
+        public bool LoggedIn { get; set; }
+    }
+}
