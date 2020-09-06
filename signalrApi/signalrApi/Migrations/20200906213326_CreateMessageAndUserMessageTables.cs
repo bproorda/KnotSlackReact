683550d5e979ee6c95c901122aa220a656a026d7
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-06 16:52:43 -0500

    created Imessage repo

---
+++ signalrApi/signalrApi/Migrations/20200906213326_CreateMessageAndUserMessageTables.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace signalrApi.Migrations
+{
+    public partial class CreateMessageAndUserMessageTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    Sender = table.Column<string>(nullable: true),
+                    Recipient = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Contents = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMessages",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    MessageId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMessages", x => new { x.UserId, x.MessageId });
+                    table.ForeignKey(
+                        name: "FK_UserMessages_Messages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "Messages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserMessages_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMessages_MessageId",
+                table: "UserMessages",
+                column: "MessageId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserMessages");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
---
+++ signalrApi/signalrApi/Repositories/MessageRepos/IMessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using signalrApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalrApi.Repositories.MessageRepos
+{
+    public interface IMessageRepository
+    {
+        Task<IEnumerable<Message>> GetMessagesByRecipient(string Recipient );
+        Task<IEnumerable<Message>> GetMessagesBySender(string User);
+
+        Task<IEnumerable<Message>> GetPrivateMessages(string UserA, string UserB);
+
+        Task<bool> CreateNewMessage(Message message);
+    }
+}
