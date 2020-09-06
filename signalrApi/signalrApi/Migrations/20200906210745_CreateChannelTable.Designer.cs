688add4edbe45244cb3d658c4e10f7f4335c0aed
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-06 16:18:19 -0500

    migrations/updated db for channels, userchannels

--- signalrApi/signalrApi/Migrations/20200906210745_CreateChannelTable.Designer.cs
+++ signalrApi/signalrApi/Migrations/20200906210745_CreateChannelTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using signalrApi.Data;
 
 namespace signalrApi.Migrations
 {
     [DbContext(typeof(knotSlackDbContext))]
-    partial class knotSlackDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200906210745_CreateChannelTable")]
+    partial class CreateChannelTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
