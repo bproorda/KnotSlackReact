abd016934cb2e4b8dbb4bd49e846431fe2f60a9c
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-06 19:04:33 -0500

    added new userLogDTO

---
+++ signalrApi/signalrApi/Models/DTO/userLogDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalrApi.Models.DTO
+{
+    public class userLogDTO
+    {
+        public string Username { get; set; }
+
+        public string[] Channels { get; set; }
+    }
+}
