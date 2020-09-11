86cc6b95d2d80f85b5dd151ca7be1d1894fc6262
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-09-11 16:39:03 -0500

    fixed dynamic styles of user list buttons

--- reactsignalr/src/components/userList/index.js
+++ reactsignalr/src/components/userList/index.js
@@ -2,16 +2,27 @@ import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import HubContext from '../../contexts/hubContext';
 
+
 export default function UserList(props) {
 
     const { allUsers } = useContext(HubContext);
+
+    const getStyle = (user) => {
+        var style = null;
+        if(user.loggedIn){
+            style = {color: "white", backgroundColor: "blue"};
+        } else {
+            style = {color: "black", backgroundColor: "lightgray"};
+        };
+        return style;
+    }
 
 
     return (
         <>
             <ul style={{ listStyleType: "none" }}>
                 {(allUsers !== null) ? allUsers.map((user, index) => (
-                    <li key={index}><Button variant={user.LoggedIn ? "primary" : "outline-primary"}>{user.username}</Button></li>
+                    <li key={index}><Button style={getStyle(user)}>{user.username}</Button></li>
                 )) : null}
             </ul>
         </>
