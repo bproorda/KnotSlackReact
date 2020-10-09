2004fd02b4362e00019f0bd6fcd28dd1e589d958
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-10-09 16:08:09 -0500

    oops, already had that fetch set up

--- reactsignalr/src/components/createGroup/index.js
+++ reactsignalr/src/components/createGroup/index.js
@@ -16,38 +16,22 @@ export default function CreateGroup(props) {
     const viewForm = () => {
         setToggle(!toggle);
     };
-    
+
     const changeHandler = (e) => {
         setGroupName(e.target.value);
     };
 
     const submitHandler = async () => {
-        const result = await fetch(`${channelsAPI}`, {
-            method: 'post',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${this.context.token}`
-            },
-            body: JSON.stringify({ name: groupName, type: "Group" }),
-          });
-      
-          //const body = await result.json();
-          //console.log(body);
-      
-          if (result.ok) {
-             doesWindowAlreadyExist(groupName, "Group");
-          } else {
-            return false;
-          }
+        await doesWindowAlreadyExist(groupName, "Group");
     };
 
     return (
         <>
-        <Button onClick={viewForm}>Create a New Group </Button>
-        {toggle ? <form onSubmit={submitHandler}>
-            <label>Name: </label>
-            <input type="text" onChange={changeHandler}></input>
-            <button type="submit">Submit</button>
+            <Button onClick={viewForm}>Create a New Group </Button>
+            {toggle ? <form onSubmit={submitHandler}>
+                <label>Name: </label>
+                <input type="text" onChange={changeHandler}></input>
+                <button type="submit">Submit</button>
             </form> : null}
         </>
     )
