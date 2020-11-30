c48f8d99a28e6192d4a63907acc506a9df984b6b
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-11-29 22:36:06 -0600

    added build folder

---
+++ reactsignalr/build/precache-manifest.fb6886f965b0f3fbcca4637c86eb187d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44be5d7366a28e52b12ed9bbd3e59f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "267d473112dfb9785fb9",
+    "url": "/static/css/main.20f9fabb.chunk.css"
+  },
+  {
+    "revision": "70cd0e687613a0cd67fb",
+    "url": "/static/js/2.f332b900.chunk.js"
+  },
+  {
+    "revision": "06baabb837e8376f57133252de69d13e",
+    "url": "/static/js/2.f332b900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267d473112dfb9785fb9",
+    "url": "/static/js/main.f1a10023.chunk.js"
+  },
+  {
+    "revision": "0ed7fef95e5e0fc314b9",
+    "url": "/static/js/runtime-main.c9fc73a6.js"
+  },
+  {
+    "revision": "5f5460c14cc39a6c72ed3eec96426f68",
+    "url": "/static/media/KnotLogo.5f5460c1.png"
+  }
+]);
