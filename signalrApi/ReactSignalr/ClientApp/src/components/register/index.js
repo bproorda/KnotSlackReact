a8fb20f4403b24b9b2cd476af0d60c39f0a53ff1
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-11-29 22:05:15 -0600

    pruning

--- signalrApi/ReactSignalr/ClientApp/src/components/register/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import useAuth from '../../contexts/auth';
-import Button from 'react-bootstrap/Button';
-import { useHistory } from "react-router-dom";
-
-export default function Register(){
-    
-}
